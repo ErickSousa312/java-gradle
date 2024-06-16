834e7401dc57d076d1a8b0934803cbd0853a86ee
ErickSousa312 <58636303+ErickSousa312@users.noreply.github.com>
2024-06-16 12:19:16 -0300

    Generics supertype

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<?> list = new ArrayList<Object>();
+        List<Integer> list2 = new ArrayList<Integer>();
+        list2.add(1);
+        list2.add(2);
+        list = list2;
+        System.out.println(list);
+
         System.out.println("Hello world!4");
     }
 }
