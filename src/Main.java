f36ac06723736d16c7505f480e47791b4e069e45
ErickSousa312 <58636303+ErickSousa312@users.noreply.github.com>
2024-06-16 14:10:25 -0300

    contravariancia

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
+import javax.swing.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<?> list = new ArrayList<Object>();
-        List<Integer> list2 = new ArrayList<Integer>();
-        list2.add(1);
-        list2.add(2);
-        list = list2;
-        System.out.println(list);
+        try {
+            List<Object> list = new ArrayList<Object>();
+            list.add("maria");
+            list.add("john");
+            List<? super Number> teste22 = list;
+            teste22.add(2);
+            Object receiver = teste22.get(2);
+            System.out.println(receiver);
 
-        System.out.println("Hello world!4");
+            List<Double> doubles = Arrays.asList(1.0, 2.0, 3.0);
+            List<Integer> integer = new ArrayList<Integer>();
+            integer.add(1);
+            integer.add(4);
+            List<? extends Number> number = integer;
+            System.out.println(number.get(0));
+            System.out.println("Hello world!4");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
