7cd1697e27244fb131b1bbc11ab3940f30175b17
ErickSousa312 <58636303+ErickSousa312@users.noreply.github.com>
2024-06-17 00:11:53 -0300

    add dependencies

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,13 @@ import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import org.silentsoft.stopwatch.Stopwatch;
 
 public class Main {
     public static void main(String[] args) {
         try {
+            Stopwatch time = new Stopwatch();
+            time.start("Array object");
             List<Object> list = new ArrayList<Object>();
             list.add("maria");
             list.add("john");
@@ -13,14 +16,16 @@ public class Main {
             teste22.add(2);
             Object receiver = teste22.get(2);
             System.out.println(receiver);
-
-            List<Double> doubles = Arrays.asList(1.0, 2.0, 3.0);
+            time.stop();
+            time.start("asList");
             List<Integer> integer = new ArrayList<Integer>();
             integer.add(1);
             integer.add(4);
             List<? extends Number> number = integer;
-            System.out.println(number.get(0));
-            System.out.println("Hello world!4");
+            Number number2 = number.get(0);
+            System.out.println(number2);
+            time.stop();
+            time.print();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
